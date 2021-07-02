4166eff1d7fde715c96b3b90722d7ea4af4a69a9
Jason Estey <esteyj@yahoo.com>
2021-07-02 13:37:02 -0700

    added clockwise point procs

--- src/geometry2d.nim
+++ src/geometry2d.nim
@@ -1,5 +1,4 @@
-
-let NaNf* = cast[float32](NaN)
+import math
 
 type
   Point* = tuple
@@ -7,7 +6,16 @@ type
     y: float32
 
 proc slope*(p1: Point, p2: Point): float32 =
-  let dx = p2.x - p1.x
-  if dx == 0:
-    return NaNf
-  return (p2.y - p1.y) / dx
+  return (p2.y - p1.y) / (p2.x - p1.x)
+
+proc slowIsClockwise*(p1: Point, p2: Point, p3: Point): bool =
+  # Reference implementation; easy to grok, but does unnecessary branches and divides.
+  # Handling of edge cases where points share an x-coordinate is different than is_clockwise.
+  let slope1 = slope(p1, p2)
+  let slope2 = slope(p1, p3)
+  if classify(slope1) == fcInf or classify(slope1) == fcNegInf:
+    return true
+  return slope1 > slope2
+
+proc isClockwise*(p1: Point, p2: Point, p3: Point): bool =
+  return (p2.y - p1.y) * (p3.x - p1.x) > (p3.y - p1.y) * (p2.x - p1.x)
--- src/geometry2d_test.nim
+++ src/geometry2d_test.nim
@@ -1,11 +1,15 @@
+import math
 import geometry2d
 
+let Inff* = 1f / 0f
+let NegInff = -1f / 0f
+
 proc testSlope =
-  let testCases = [
+  let tests = [
     ((0f, 0f), (1f, 0f), 0f),
     ((0f, 0f), (1f, 1f), 1f),
-    ((0f, 0f), (0f, 1f), NaNf),
-    ((1f, 1f), (1f, 0f), NaNf),
+    ((0f, 0f), (0f, 1f), Inff),
+    ((1f, 1f), (1f, 0f), NegInff),
     ((0f, 0f), (1f, 3f), 3f),
     ((0f, 0f), (-1f, 3f), -3f),
     ((0f, 0f), (-1f, -3f), 3f),
@@ -13,12 +17,40 @@ proc testSlope =
     ((-1f, -1f), (-2f, -2f), 1f),
     ((1f, 1f), (2f, -2f), -3f),
   ]
-  for testCase in testCases:
-    let p1 = testCase[0]
-    let p2 = testCase[1]
-    let expected = testCase[2]
+  for test in tests:
+    let p1 = test[0]
+    let p2 = test[1]
+    let expected = test[2]
     let result = p1.slope(p2)
+    if expected == Inff:
+      doAssert classify(result) == fcInf
+    elif expected == NegInff:
+      doAssert classify(result) == fcNegInf
+    else:
+      doAssert result == expected
+
+proc testIsClockwise =
+  let tests = [
+    ((0f, 0f), (0f, 0f), (0f, 0f), false),
+    ((0f, 0f), (0f, 0f), (1f, 1f), false),
+    ((0f, 0f), (0f, 0f), (1f, -1f), false),
+    ((0f, 0f), (1f, 1f), (0f, 0f), false),
+    ((0f, 0f), (1f, -1f), (0f, 0f), false),
+    ((0f, 0f), (1f, 0f), (1f, -1f), true),
+    ((0f, 0f), (1f, 0f), (1f, 1f), false),
+    ((1f, 1f), (1f, 0f), (0f, 0f), true),
+    ((5f, 5f), (6f, 4f), (5f, 3f), true),
+    ((5f, 5f), (6f, 4f), (7f, 3f), false),
+  ]
+  for test in tests:
+    let p1 = test[0]
+    let p2 = test[1]
+    let p3 = test[2]
+    let expected = test[3]
+    let result = isClockwise(p1, p2, p3)
     doAssert(result == expected)
+    doAssert(result == slowIsClockwise(p1, p2, p3))
 
 when isMainModule:
   testSlope()
+  testIsClockwise()
