606acc308188fba3741f5950e0053f2476df4e51
Jason Estey <esteyj@yahoo.com>
2021-07-02 13:49:30 -0700

    added line, valueAt

--- src/geometry2d.nim
+++ src/geometry2d.nim
@@ -8,14 +8,13 @@ type
 proc slope*(p1: Point, p2: Point): float32 =
   return (p2.y - p1.y) / (p2.x - p1.x)
 
-proc slowIsClockwise*(p1: Point, p2: Point, p3: Point): bool =
-  # Reference implementation; easy to grok, but does unnecessary branches and divides.
-  # Handling of edge cases where points share an x-coordinate is different than is_clockwise.
-  let slope1 = slope(p1, p2)
-  let slope2 = slope(p1, p3)
-  if classify(slope1) == fcInf or classify(slope1) == fcNegInf:
-    return true
-  return slope1 > slope2
-
 proc isClockwise*(p1: Point, p2: Point, p3: Point): bool =
   return (p2.y - p1.y) * (p3.x - p1.x) > (p3.y - p1.y) * (p2.x - p1.x)
+
+type
+  Line* = tuple
+    yIntercept: float32
+    slope: float32
+
+proc valueAt*(line: Line, x: float32): float32 =
+  return line.slope * x + line.yIntercept
--- src/geometry2d_test.nim
+++ src/geometry2d_test.nim
@@ -6,6 +6,7 @@ let NegInff = -1f / 0f
 
 proc testSlope =
   let tests = [
+    # (point1, point2, slope)
     ((0f, 0f), (1f, 0f), 0f),
     ((0f, 0f), (1f, 1f), 1f),
     ((0f, 0f), (0f, 1f), Inff),
@@ -29,8 +30,18 @@ proc testSlope =
     else:
       doAssert result == expected
 
+proc slowIsClockwise(p1: Point, p2: Point, p3: Point): bool =
+  # Reference implementation; easy to grok, but does unnecessary branches and divides.
+  # Handling of edge cases where points share an x-coordinate is different than is_clockwise.
+  let slope1 = slope(p1, p2)
+  let slope2 = slope(p1, p3)
+  if classify(slope1) == fcInf or classify(slope1) == fcNegInf:
+    return true
+  return slope1 > slope2
+
 proc testIsClockwise =
   let tests = [
+    # (point1, point2, point3, isClockwise)
     ((0f, 0f), (0f, 0f), (0f, 0f), false),
     ((0f, 0f), (0f, 0f), (1f, 1f), false),
     ((0f, 0f), (0f, 0f), (1f, -1f), false),
@@ -51,6 +62,22 @@ proc testIsClockwise =
     doAssert result == expected
     doAssert result == slowIsClockwise(p1, p2, p3)
 
+proc testValueAt =
+  let tests = [
+    # ((yIntercept, slope), x, valueAt)
+    ((0f, 1f), 3f, 3f),
+    ((0f, -1f), 3f, -3f),
+    ((1f, 0f), 3f, 1f),
+    ((-1f, 0f), 3f, -1f),
+  ]
+  for test in tests:
+    let line = test[0]
+    let x = test[1]
+    let expected = test[2]
+    let result = valueAt(line, x)
+    doAssert result == expected
+
 when isMainModule:
   testSlope()
   testIsClockwise()
+  testValueAt()
