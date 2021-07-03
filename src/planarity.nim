bd4d3d5db67ee812c23bce3a00e0764e84cc44cf
Jason Estey <esteyj@yahoo.com>
2021-07-03 10:48:16 -0700

    added empty SDL window

--- src/planarity.nim
+++ src/planarity.nim
@@ -1,5 +1,53 @@
-# This is just an example to get you started. A typical binary package
-# uses this file as the main entry point of the application.
+import sdl2
+
+const
+  WindowWidth = 800
+  WindowHeight = 600
+
+type SDLException = object of Defect
+
+template sdlFailIf(condition: typed, reason: string) =
+  if condition: raise SDLException.newException(
+    reason & ", SDL error " & $getError()
+  )
+
+proc main =
+  sdlFailIf(not sdl2.init(INIT_VIDEO or INIT_TIMER or INIT_EVENTS)):
+    "SDL2 initialization failed"
+  defer: sdl2.quit()
+
+  let window = createWindow(
+    title = "Planarity",
+    x = SDL_WINDOWPOS_CENTERED,
+    y = SDL_WINDOWPOS_CENTERED,
+    w = WindowWidth,
+    h = WindowHeight,
+    flags = SDL_WINDOW_SHOWN
+  )
+
+  sdlFailIf window.isNil: "window could not be created"
+  defer: window.destroy()
+
+  let renderer = createRenderer(
+    window = window,
+    index = -1,
+    flags = Renderer_Accelerated or Renderer_PresentVsync or Renderer_TargetTexture
+  )
+  sdlFailIf renderer.isNil: "renderer could not be created"
+  defer: renderer.destroy()
+
+  var running = true
+
+  while running:
+    var event = defaultEvent
+
+    while pollEvent(event):
+      case event.kind
+      of QuitEvent:
+        running = false
+        break
+      else:
+        discard
 
 when isMainModule:
-  echo("Hello, World!")
+  main()
